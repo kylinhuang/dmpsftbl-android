1c4057bbf5280191a6c1ca21a75cce4d084e1391
Andy <baumann-andy@gmx.de>
2012-09-03 01:17:06 +0200

    Tabviews created

---
+++ Damps/src/de/damps/fantasy/InboxActivity.java
@@ -0,0 +1,15 @@
+package de.damps.fantasy;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+
+public class InboxActivity extends ListActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.inbox);
+	}
+
+
+}
--- Damps/src/de/damps/fantasy/MessagesActivity.java
+++ Damps/src/de/damps/fantasy/MessagesActivity.java
@@ -1,16 +1,36 @@
 package de.damps.fantasy;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MessagesActivity extends Activity {
+public class MessagesActivity extends TabActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.messages);
+
+		TabHost tabHost = getTabHost();
+
+		TabSpec inspec = tabHost.newTabSpec("Inbox");
+		inspec.setIndicator("Inbox");
+		Intent inintent = new Intent(getApplicationContext(), InboxActivity.class);
+		inspec.setContent(inintent);
+
+		TabSpec outspec = tabHost.newTabSpec("Outbox");
+		outspec.setIndicator("Outbox");
+		Intent outintent = new Intent(getApplicationContext(), OutboxActivity.class);
+		outspec.setContent(outintent);
+		
+		tabHost.addTab(inspec);
+		tabHost.addTab(outspec);
+		tabHost.setCurrentTab(0);
+
+
 	}
 
 	public void sendMessage(View view) {
