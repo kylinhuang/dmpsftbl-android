8a4be7d55aefc0ab42232d02203923a622e4f6de
Andy <baumann-andy@gmx.de>
2012-09-19 17:16:03 +0200

    setformation

--- Damps/src/de/damps/fantasy/activities/SetStartersActivity.java
+++ Damps/src/de/damps/fantasy/activities/SetStartersActivity.java
@@ -38,6 +38,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import de.damps.fantasy.R;
+import de.damps.fantasy.data.DataPost;
 import de.damps.fantasy.data.Json;
 import de.damps.fantasy.data.Player;
 import de.damps.fantasy.data.Row;
@@ -52,17 +53,38 @@ public class SetStartersActivity extends Activity {
 	private String url;
 	private String url_start;
 	private String url_bench;
+	private String url_formation;
+	
+
 	private TextView team;
+	private TableLayout head;
+
+	private SharedPreferences pref;
 	private String token;
 	private String hash;
-	private SharedPreferences pref;
+
 	private String dteam;
 	private String oid;
+	private Integer starter_id;
+
 	private int gd;
 	private boolean firstgd;
-	private boolean init = false;
-	private TableLayout head;
-	private Integer starter_id;
+	private boolean init_gd = false;
+
+	private boolean flex_possible;
+	private boolean flex;
+	private int st_i1;
+	private int st_i2;
+	private int f_i1;
+	private int f_i2;
+	private String f_p1 = null;
+	private String f_p2 = null;
+	private String st_p1;
+	private String st_p2;
+	private int flex_for;
+	private int my_flex_for;
+	private int[] formations = { 0, 311, 131, 122, 212, 113 };
+	private int teamstarter_id;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -90,6 +112,8 @@ public class SetStartersActivity extends Activity {
 				+ "/setstarter/";
 		url_bench = de.damps.fantasy.activities.HomeActivity.URL
 				+ "/removestarter/";
+		url_formation = de.damps.fantasy.activities.HomeActivity.URL
+				+ "/setformation/";
 
 		team = (TextView) findViewById(R.id.tv_setstarters_title);
 		dteam = de.damps.fantasy.activities.HomeActivity.league
@@ -99,56 +123,6 @@ public class SetStartersActivity extends Activity {
 		tbl = (TableLayout) findViewById(R.id.tl_setstarters_roster);
 
 		new GetTeam().execute();
-	}
-
-	private void initializeSpinner() {
-		ArrayList<String> gamedays = new ArrayList<String>(
-				Arrays.asList(getResources().getStringArray(R.array.gameday)));
-		for (int i = 0; i < gd - 1; i++) {
-			gamedays.remove(0);
-		}
-		sp_gd = (Spinner) findViewById(R.id.spi_setstarters_gameday);
-		ArrayAdapter<String> ad_gd = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, gamedays);
-		ad_gd.setDropDownViewResource(android.R.layout.simple_spinner_item);
-		sp_gd.setAdapter(ad_gd);
-
-		sp_gd.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			@Override
-			public void onItemSelected(AdapterView<?> parent, View view,
-					int position, long id) {
-				if (init) {
-					String gameday = ((String) sp_gd.getSelectedItem())
-							.substring(8);
-					url = de.damps.fantasy.activities.HomeActivity.URL
-							+ "/myteamstarter/" + gameday;
-					new GetTeam().execute();
-				}
-				init = true;
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
-
-		});
-
-		ArrayList<String> formations = new ArrayList<String>(
-				Arrays.asList(getResources().getStringArray(R.array.formations)));
-
-		sp_for = (Spinner) findViewById(R.id.spi_setstarters_formation);
-		ArrayAdapter<String> ad_for = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, formations);
-		ad_for.setDropDownViewResource(android.R.layout.simple_spinner_item);
-		sp_for.setAdapter(ad_for);
-	}
-
-	/*
-	 * return to last screen
-	 */
-	public void back(View view) {
-		finish();
 	}
 
 	/*
@@ -172,75 +146,16 @@ public class SetStartersActivity extends Activity {
 		@Override
 		protected void onPostExecute(Void v) {
 			contructTeam();
-			fillBench();
-			if (!init) {
+
+			if (!init_gd) {
 				initializeSpinner();
 			}
+			sp_for.setSelection(my_flex_for);
+			sp_for.setClickable((flex_possible | flex));
 			fillStarter();
+			fillBench();
 			pb.setVisibility(View.INVISIBLE);
 		}
-	}
-
-	/*
-	 * start player
-	 */
-	private class SendStarter extends AsyncTask<String, Void, Boolean> {
-
-		@Override
-		protected void onPreExecute() {
-		};
-
-		@Override
-		protected Boolean doInBackground(String... params) {
-			return startPlayer(params);
-		}
-
-		protected void onPostExecute(Boolean result) {
-
-		}
-	}
-
-	private boolean startPlayer(String[] data) {
-		final DefaultHttpClient client = new DefaultHttpClient();
-		String url_send = data[0];
-		final HttpPost httppost = new HttpPost(url_send);
-		final List<NameValuePair> postPara = new ArrayList<NameValuePair>();
-		
-		postPara.add(new BasicNameValuePair("token", token));
-		postPara.add(new BasicNameValuePair("hash", hash));
-		
-		if(data.length == 5){
-			postPara.add(new BasicNameValuePair("season", data[1]));
-			postPara.add(new BasicNameValuePair("gameday", data[2]));
-			postPara.add(new BasicNameValuePair("position", data[3]));
-			postPara.add(new BasicNameValuePair("roster_id", data[4]));
-		}else{
-			postPara.add(new BasicNameValuePair("starter_id", data[1]));
-		}
-		
-		
-
-		try {
-			httppost.setEntity(new UrlEncodedFormEntity(postPara));
-			try {
-				HttpResponse response = client.execute(httppost);
-				String s = EntityUtils.toString(response.getEntity());
-				if (false) {
-					return false;
-				}else{
-					if(!s.equals("true")){
-						starter_id = Integer.valueOf(s);
-					}
-				}
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		return true;
 	}
 
 	/*
@@ -251,6 +166,42 @@ public class SetStartersActivity extends Activity {
 		JSONObject jo = data.data;
 
 		try {
+			if (jo.getInt("flexcount") < 3) {
+				flex_possible = true;
+			}
+
+			flex_for = jo.getJSONArray("Starter").getJSONObject(0)
+					.getJSONObject("Teamstarter").getInt("is_flex");
+			if (flex_for == 0) {
+				flex = false;
+			} else {
+				flex = true;
+			}
+
+			teamstarter_id = jo.getJSONArray("Starter").getJSONObject(0)
+					.getJSONObject("Teamstarter").getInt("id");
+
+			switch (flex_for) {
+			case 0:
+				my_flex_for = 0;
+				break;
+			case 311:
+				my_flex_for = 1;
+				break;
+			case 131:
+				my_flex_for = 2;
+				break;
+			case 122:
+				my_flex_for = 3;
+				break;
+			case 212:
+				my_flex_for = 4;
+				break;
+			case 113:
+				my_flex_for = 5;
+				break;
+			}
+
 			if (!firstgd) {
 				gd = jo.getInt("Gameday");
 				firstgd = true;
@@ -276,6 +227,260 @@ public class SetStartersActivity extends Activity {
 			e.printStackTrace();
 		}
 
+	}
+
+	private void initializeSpinner() {
+		ArrayList<String> gamedays = new ArrayList<String>(
+				Arrays.asList(getResources().getStringArray(R.array.gameday)));
+		for (int i = 0; i < gd - 1; i++) {
+			gamedays.remove(0);
+		}
+		sp_gd = (Spinner) findViewById(R.id.spi_setstarters_gameday);
+		ArrayAdapter<String> ad_gd = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, gamedays);
+		ad_gd.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		sp_gd.setAdapter(ad_gd);
+
+		sp_gd.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (init_gd) {
+					String gameday = ((String) sp_gd.getSelectedItem())
+							.substring(8);
+					url = de.damps.fantasy.activities.HomeActivity.URL
+							+ "/myteamstarter/" + gameday;
+					f_p1 = null;
+					f_p2 = null;
+					new GetTeam().execute();
+				}
+				init_gd = true;
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+
+		});
+
+		ArrayList<String> formations = new ArrayList<String>(
+				Arrays.asList(getResources().getStringArray(R.array.formations)));
+
+		sp_for = (Spinner) findViewById(R.id.spi_setstarters_formation);
+		ArrayAdapter<String> ad_for = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, formations);
+		ad_for.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		sp_for.setAdapter(ad_for);
+
+		sp_for.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				flexFormation(sp_for.getSelectedItemPosition());
+				if (sp_for.getSelectedItemPosition() == 0) {
+					flex = false;
+				} else {
+					flex = true;
+				}
+				new SendFormation().execute();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
+
+	}
+
+	private void flexFormation(int f) {
+
+		if (f_p1 != null) {
+			TableRow oldRow = (TableRow) tbl.getChildAt(f_i1);
+			if ((Boolean) oldRow.getTag()) {
+				benchPlayer(oldRow);
+			} else {
+				tbl.removeViewAt(f_i1);
+			}
+
+			tbl.addView(new Row(getApplicationContext(), st_p1, head).newRow,
+					st_i1);
+		}
+
+		if (f_p2 != null) {
+			TableRow oldRow = (TableRow) tbl.getChildAt(f_i2);
+			if ((Boolean) oldRow.getTag()) {
+				benchPlayer(oldRow);
+			} else {
+				tbl.removeViewAt(f_i2);
+			}
+
+			tbl.addView(new Row(getApplicationContext(), st_p2, head).newRow,
+					st_i2);
+		}
+
+		switch (f) {
+		case 0:
+			f_p1 = null;
+			f_p1 = null;
+			return;
+		case 1:
+			st_i1 = 4;
+			f_i1 = 3;
+			st_p1 = "WR";
+			f_p1 = "RB";
+			break;
+		case 2:
+			st_i1 = 2;
+			f_i1 = 4;
+			st_p1 = "RB";
+			f_p1 = "WR";
+			break;
+		case 3:
+			st_i1 = 2;
+			f_i1 = 5;
+			st_p1 = "RB";
+			f_p1 = "TE";
+			break;
+		case 4:
+			st_i1 = 4;
+			f_i1 = 5;
+			st_p1 = "WR";
+			f_p1 = "TE";
+			break;
+		case 5:
+			st_i1 = 3;
+			f_i1 = 5;
+
+			st_i2 = 2;
+			f_i2 = 5;
+
+			st_p1 = "WR";
+			f_p1 = "TE";
+
+			st_p2 = "RB";
+			f_p2 = "TE";
+
+			TableRow oldRow = (TableRow) tbl.getChildAt(st_i2);
+			if ((Boolean) oldRow.getTag()) {
+				benchPlayer(oldRow);
+			} else {
+				tbl.removeViewAt(st_i2);
+			}
+
+			tbl.addView(new Row(getApplicationContext(), f_p2, head).newRow,
+					f_i2);
+			break;
+		}
+
+		TableRow oldRow = (TableRow) tbl.getChildAt(st_i1);
+		if ((Boolean) oldRow.getTag()) {
+			benchPlayer(oldRow);
+		} else {
+			tbl.removeViewAt(st_i1);
+		}
+
+		tbl.addView(new Row(getApplicationContext(), f_p1, head).newRow, f_i1);
+	}
+
+	/*
+	 * return to last screen
+	 */
+	public void back(View view) {
+		finish();
+	}
+
+	private class SendFormation extends AsyncTask<Void, Void, Void> {
+		String[][] data = new String[2][2];
+
+		@Override
+		protected void onPreExecute() {
+			data[0][0] = "is_flex";
+			data[0][1] = ((Integer) formations[my_flex_for]).toString();
+			data[1][0] = "id";
+			data[1][1] = ((Integer) teamstarter_id).toString();
+		};
+
+		@Override
+		protected Void doInBackground(Void... v) {
+			new DataPost(url_formation, data);
+			return null;
+		}
+
+	}
+
+	/*
+	 * start player
+	 */
+	private class SendBench extends AsyncTask<Void, Void, Void> {
+		String[][] data = new String[1][2];
+
+		@Override
+		protected void onPreExecute() {
+			data[0][0] = "starter_id";
+			data[0][1] = ((Integer) starter_id).toString();
+		};
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			new DataPost(url_bench, data);
+			return null;
+		}
+
+	}
+
+	/*
+	 * start player
+	 */
+	private class SendStarter extends AsyncTask<String, Void, Boolean> {
+
+		@Override
+		protected void onPreExecute() {
+		};
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			return startPlayer(params);
+		}
+
+	}
+
+	private boolean startPlayer(String[] data) {
+		final DefaultHttpClient client = new DefaultHttpClient();
+		String url_send = data[0];
+		final HttpPost httppost = new HttpPost(url_send);
+		final List<NameValuePair> postPara = new ArrayList<NameValuePair>();
+
+		postPara.add(new BasicNameValuePair("token", token));
+		postPara.add(new BasicNameValuePair("hash", hash));
+
+		postPara.add(new BasicNameValuePair("season", data[1]));
+		postPara.add(new BasicNameValuePair("gameday", data[2]));
+		postPara.add(new BasicNameValuePair("position", data[3]));
+		postPara.add(new BasicNameValuePair("roster_id", data[4]));
+
+		try {
+			httppost.setEntity(new UrlEncodedFormEntity(postPara));
+			try {
+				HttpResponse response = client.execute(httppost);
+				String s = EntityUtils.toString(response.getEntity());
+				if (false) {
+					return false;
+				} else {
+					if (!s.equals("true")) {
+						starter_id = Integer.valueOf(s);
+					}
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return true;
 	}
 
 	/*
@@ -327,6 +532,7 @@ public class SetStartersActivity extends Activity {
 
 			tbl.addView(newRowBench, i);
 		}
+
 	}
 
 	private OnClickListener starterListener(final int i) {
@@ -360,50 +566,15 @@ public class SetStartersActivity extends Activity {
 	private void startPlayer(TableRow v) {
 
 		String pos = (String) ((TextView) v.getVirtualChildAt(1)).getText();
-		int j = 0;
-		if (pos.equals("QB")
-				&& !(Boolean) ((TableRow) tbl.getChildAt(0)).getTag()) {
 
-			j = 0;
-			start(j, v);
-		} else if (pos.equals("RB")) {
-			if (!(Boolean) ((TableRow) tbl.getChildAt(1)).getTag()
-					|| !(Boolean) ((TableRow) tbl.getChildAt(2)).getTag()) {
-
-				if ((Boolean) ((TableRow) tbl.getChildAt(1)).getTag()) {
-					j = 2;
-					start(j, v);
-				} else {
-					j = 1;
-					start(j, v);
-				}
+		for (int j = 0; j < 9; j++) {
+			TableRow row = (TableRow) tbl.getChildAt(j);
+			if (((TextView) row.getVirtualChildAt(1)).getText().equals(pos)
+					&& !(Boolean) row.getTag()) {
+				start(j, v);
+				return;
 			}
-		} else if (pos.equals("WR")) {
-			if (!(Boolean) ((TableRow) tbl.getChildAt(3)).getTag()
-					|| !(Boolean) ((TableRow) tbl.getChildAt(4)).getTag()) {
-
-				if ((Boolean) ((TableRow) tbl.getChildAt(3)).getTag()) {
-					j = 4;
-					start(j, v);
-				} else {
-					j = 3;
-					start(j, v);
-				}
-			}
-		} else if (pos.equals("TE")
-				&& !(Boolean) ((TableRow) tbl.getChildAt(5)).getTag()) {
-			j = 5;
-			start(j, v);
-		} else if (pos.equals("K")
-				&& !(Boolean) ((TableRow) tbl.getChildAt(6)).getTag()) {
-			j = 6;
-			start(j, v);
-		} else if (pos.equals("DEF")
-				&& !(Boolean) ((TableRow) tbl.getChildAt(7)).getTag()) {
-			j = 7;
-			start(j, v);
 		}
-
 	}
 
 	private void start(int j, TableRow v) {
@@ -447,7 +618,7 @@ public class SetStartersActivity extends Activity {
 					data[2] = ((Integer) gd).toString();
 					data[3] = p.pos;
 					data[4] = ((Integer) p.roster_id).toString();
-					
+
 					boolean locked = true;
 					try {
 						locked = !new SendStarter().execute(data).get();
@@ -466,11 +637,8 @@ public class SetStartersActivity extends Activity {
 				if (pos.equals(from.get(i).pos)
 						&& name.equals(from.get(i).name)) {
 					Player p = from.get(i);
-					String[] data = new String[2];
-					data[0] = url_bench;
-					data[1] =  ((Integer)p.starter_id).toString();
-					
-					new SendStarter().execute(data);
+					starter_id = p.starter_id;
+					new SendBench().execute();
 				}
 			}
 		}
@@ -482,42 +650,15 @@ public class SetStartersActivity extends Activity {
 	 */
 	private void fillStarter() {
 		for (int i = 0; i < starter.size(); i++) {
-			int j = 0;
-			/*
-			 * if (i % 2 == 1) { tbl.getChildAt(i).setBackgroundColor(
-			 * getResources().getColor(R.color.hellhellgrau)); }
-			 */
-			String pos = starter.get(i).pos;
-			if (pos.equals("QB")) {
-				j = 0;
-				fillRow(i, j);
-			} else if (pos.equals("RB")) {
-				if ((Boolean) ((TableRow) tbl.getChildAt(1)).getTag()) {
-					j = 2;
-					fillRow(i, j);
-				} else {
-					j = 1;
-					fillRow(i, j);
+			for (int k = 0; k < 9; k++) {
+				String pos = starter.get(i).pos;
+				TableRow row = (TableRow) tbl.getChildAt(k);
+				if (((TextView) row.getVirtualChildAt(1)).getText().equals(pos)
+						&& !(Boolean) row.getTag()) {
+					fillRow(i, k);
+					break;
 				}
-			} else if (pos.equals("WR")) {
-				if ((Boolean) ((TableRow) tbl.getChildAt(3)).getTag()) {
-					j = 4;
-					fillRow(i, j);
-				} else {
-					j = 3;
-					fillRow(i, j);
-				}
-			} else if (pos.equals("TE")) {
-				j = 5;
-				fillRow(i, j);
-			} else if (pos.equals("K")) {
-				j = 6;
-				fillRow(i, j);
-			} else if (pos.equals("DEF")) {
-				j = 7;
-				fillRow(i, j);
 			}
-
 		}
 	}
 
