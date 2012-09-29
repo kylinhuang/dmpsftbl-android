d3906b8c9bcc05081c9540c7867f637dcf5d80e8
Andy <baumann-andy@gmx.de>
2012-09-29 12:51:39 +0200

    Löschfunktion für Nachrichten

--- Damps/src/de/damps/fantasy/activities/InboxActivity.java
+++ Damps/src/de/damps/fantasy/activities/InboxActivity.java
@@ -1,6 +1,7 @@
 package de.damps.fantasy.activities;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -15,15 +16,35 @@ import de.damps.fantasy.data.Message;
 
 public class InboxActivity extends ListActivity {
 
-	/*
-	 * mark message as read
-	 */
+	private class DeleteMessage extends AsyncTask<Integer, Void, Boolean> {
+		String[][] data = new String[1][2];
+
+		@Override
+		protected Boolean doInBackground(Integer... i) {
+			data[0][0] = "id";
+			data[0][1] = i[0].toString();
+			String response = new DataPost(url_delete, data).response;
+			if(response.equals("true")){
+				return true;
+			}
+			return false;
+		};
+
+		@Override
+		protected void onPostExecute(Boolean r) {
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+	}
+
 	private class ReadMessage extends AsyncTask<Void, Void, Void> {
 		String[][] data = new String[1][2];
 
 		@Override
 		protected Void doInBackground(Void... voids) {
-			new DataPost(url, data);
+			new DataPost(url_read, data);
 			return null;
 		};
 
@@ -40,13 +61,23 @@ public class InboxActivity extends ListActivity {
 
 	private MessageAdapter messageadapter;
 	private ArrayList<Message> messages = new ArrayList<Message>();
-	private String url;
+	private String url_delete;
+	private String url_read;
 
 	private Message message;
 
-	/*
-	 * back from reading
-	 */
+	public void deleteMessage(View view) {
+		try {
+			if(new DeleteMessage().execute((Integer) view.getTag()).get()){
+				removeMessage((Integer) view.getTag());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == 1) {
@@ -61,15 +92,13 @@ public class InboxActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.inbox);
-		url = de.damps.fantasy.CommonUtilities.URL + "/readmessage";
+		url_delete = de.damps.fantasy.CommonUtilities.URL + "/deletemessage";
+		url_read = de.damps.fantasy.CommonUtilities.URL + "/readmessage";
 		messages = getIntent().getExtras().getParcelableArrayList("messages");
 
 		showThreads();
 	}
 
-	/*
-	 * open message
-	 */
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
@@ -83,9 +112,16 @@ public class InboxActivity extends ListActivity {
 		startActivityForResult(intent, 1);
 	}
 
-	/*
-	 * fill list
-	 */
+	private void removeMessage(Integer tag) {
+		for(int i = 0; i<messages.size();i++){
+			if(messages.get(i).id==tag){
+				messages.remove(i);
+				showThreads();
+				break;
+			}
+		}
+	}
+	
 	private void showThreads() {
 		messageadapter = new MessageAdapter(this, R.layout.threaditem, messages);
 		setListAdapter(messageadapter);
--- Damps/src/de/damps/fantasy/adapter/MessageAdapter.java
+++ Damps/src/de/damps/fantasy/adapter/MessageAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import de.damps.fantasy.R;
 import de.damps.fantasy.data.Message;
@@ -40,19 +41,24 @@ public class MessageAdapter extends ArrayAdapter<Message> {
 		TextView title = (TextView) v.findViewById(R.id.tv_mes_title);
 		TextView sender = (TextView) v.findViewById(R.id.tv_mes_sender);
 		ImageView image = (ImageView) v.findViewById(R.id.iv_mes_read);
+		ImageView delete = (ImageView) v.findViewById(R.id.iv_mes_delete);
 
-		// Assign the appropriate data from our alert object above
 		title.setText(m.title);
-		if (m.read[1]) {
+		
+		if (m.read[1]) {	//Outbox
+			RelativeLayout rl = (RelativeLayout) v.findViewById(R.id.rl_mes_view);
 			sender.setText(m.to);
-		} else {
+			rl.removeView(delete);
+		} else {	//Inbox
 			sender.setText(m.from);
+			delete.setTag(m.id);
 		}
 		if (m.read[0]) {
 			image.setImageResource(R.drawable.content_read);
 		} else {
 			image.setImageResource(R.drawable.content_unread);
 		}
+		
 		return v;
 	}
 
